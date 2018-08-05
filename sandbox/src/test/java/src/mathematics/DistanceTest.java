f16d549e5d52ab1b5fcbdcd5b9c1f6dff00e563e
TatyanaKrupchan <38409068+TatyanaKrupchan@users.noreply.github.com>
2018-08-05 16:51:56 +0200

    Task 3
    Fixed messages for some checks

--- sandbox/src/test/java/src/mathematics/DistanceTest.java
+++ sandbox/src/test/java/src/mathematics/DistanceTest.java
@@ -15,10 +15,10 @@ public class DistanceTest {
         Assert.assertTrue(p1.distance(p2) >= 0, "Distance between p1 and p2 < 0");
         Assert.assertEquals(p1.distance(p2), 23.870970570967574296227365337104);
 
-        Assert.assertTrue(p1.distance(p3) >= 0, "Distance between p1 and p2 < 0");
+        Assert.assertTrue(p1.distance(p3) >= 0, "Distance between p1 and p3 < 0");
         Assert.assertEquals(p1.distance(p3), 11.180339887498949);
 
-        Assert.assertTrue(p2.distance(p3) >= 0, "Distance between p1 and p2 < 0");
+        Assert.assertTrue(p2.distance(p3) >= 0, "Distance between p2 and p3 < 0");
         Assert.assertEquals(p2.distance(p3), 12.853141094689656);
 
     }
