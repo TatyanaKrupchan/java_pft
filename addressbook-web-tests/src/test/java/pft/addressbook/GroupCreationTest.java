91e7b8c72fd9a602d7c614ea9d252b7c98745290
TatyanaKrupchan <38409068+TatyanaKrupchan@users.noreply.github.com>
2018-08-11 23:09:56 +0200

    Lecture 02
    Updated the test as per lecture 2.4

--- addressbook-web-tests/src/test/java/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pft/addressbook/GroupCreationTest.java
@@ -5,53 +5,74 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTest {
     FirefoxDriver wd;
 
     @BeforeMethod
     public void setUp() throws Exception {
+        final String loginName = "admin";
+        final String loginPassword = "secret";
+
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
+        login(loginName, loginPassword);
+    }
+
+    private void login(String login, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(login);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+
     }
 
     @Test
     public void testForGroup() {
         // wd.findElement(By.xpath("//div[@id='content']//h1[.='GROUPS']")).click();
-        wd.findElement(By.linkText("GROUPS")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("Test_groupname1");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("Test_groupheader1");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("Test_comment1");
-        wd.findElement(By.name("submit")).click();
+        gotoGroupsTab();
+        initCreateNewGroup();
+        fillNewGroupData(new GroupData("Test_groupname1", "Test_groupheader1", "Test_comment1"));
+        submitNewGroupData();
+        returnBackToGroupsTab();
+    }
+
+    private void returnBackToGroupsTab() {
         wd.findElement(By.linkText("group page")).click();
     }
+
+    private void gotoGroupsTab() {
+        wd.findElement(By.linkText("GROUPS")).click();
+    }
+
+    private void submitNewGroupData() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillNewGroupData(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getGroupComment());
+    }
+
+    private void initCreateNewGroup() {
+        wd.findElement(By.name("new")).click();
+    }
+
 
     @AfterMethod
     public void tearDown() {
---
+++ addressbook-web-tests/src/test/java/pft/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package pft.addressbook;
+
+public class GroupData {
+    private final String groupName;
+    private final String groupHeader;
+    private final String groupComment;
+
+    public GroupData(String groupName, String groupHeader, String groupComment) {
+        this.groupName = groupName;
+        this.groupHeader = groupHeader;
+        this.groupComment = groupComment;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public String getGroupHeader() {
+        return groupHeader;
+    }
+
+    public String getGroupComment() {
+        return groupComment;
+    }
+}
