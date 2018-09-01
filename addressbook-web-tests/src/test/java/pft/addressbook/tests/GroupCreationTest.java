f9f2fa9aacfa91a391849e1f62017230f16befdd
TatyanaKrupchan <38409068+TatyanaKrupchan@users.noreply.github.com>
2018-09-02 00:03:48 +0200

    Lecture 4.9
    Changed sorting to lambda-function

--- addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,20 +16,22 @@ public class GroupCreationTest extends TestBase {
         app.getNavigationHelper().gotoGroupsTab();
         // int before = app.getGroupHelper().getGroupsCount();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("Test_groupname1", "Test_groupheader1", "Test_comment1");
+        GroupData group = new GroupData("Test_groupname2", "Test_groupheader2", "Test_comment2");
         app.getGroupHelper().createGroup(group);
         app.getNavigationHelper().returnBackToGroupsTab();
         // int after = app.getGroupHelper().getGroupsCount();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
+        /*int max = 0;
         for (GroupData g : after){
             if (g.getId() > 0){
                 max = g.getId();
             }
-        }
-        group.setId(max);
+        }*/
+        // Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        // int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
 
